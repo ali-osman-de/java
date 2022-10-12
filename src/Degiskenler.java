e4503b4e328a1cb7544bb1dd08c1126555d8a763
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-10-12 10:39:03 +0300

    Create Degiskenler.java

---
+++ src/Degiskenler.java
@@ -0,0 +1,10 @@
+public class Degiskenler {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 39;
+        String mesaj = "Öğrenci sayım: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım: 10");
+        //or
+        System.out.printf(mesaj +"%d\n",ogrenciSayisi);
+    }
+}
